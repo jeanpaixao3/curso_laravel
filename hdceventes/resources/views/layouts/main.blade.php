b7e5f432763a1611cf4b054e72cdddf9894d5a82
Jean Paixão <jeanpaixao3@gmail.com>
2024-04-09 21:23:07 -0300

    add font google e botstrap

--- hdceventes/resources/views/layouts/main.blade.php
+++ hdceventes/resources/views/layouts/main.blade.php
@@ -6,6 +6,16 @@
 
         <title>@yield('title')</title>
 
+        <!-- Fonte do Google-->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!--CSS Bootstrap-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+        
+
+
+        <!--CSS da aplicação-->
         <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
 
